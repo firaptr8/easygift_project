07a6681b806c264e5b1cb0770a6c0f8506a710cc
rizkyaryaputra <72532358+rizkyaryaputra@users.noreply.github.com>
2021-11-06 10:10:02 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EasyGift',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
